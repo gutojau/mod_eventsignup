940261bff8660317ffb7cfb20e0734da3d786fc2
GUTO CHRISTIANINI <43747810+gutojau@users.noreply.github.com>
2025-06-17 21:45:58 -0300

    Add files via upload

---
+++ lang/en/eventsignup.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Ficheiro de idioma inglês para o módulo eventsignup.
+ *
+ * @package   mod_eventsignup
+ * @copyright 2024 Seu Nome
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// ... (todas as strings anteriores) ...
+$string['pluginname'] = 'Event Signup';
+$string['modulename'] = 'Event Signup';
+$string['modulename_plural'] = 'Event Signups';
+// ...
+
+// Gestão de Perguntas
+$string['editquestions'] = 'Editar Perguntas';
+$string['confirmdeletequestion'] = 'Tem a certeza de que quer excluir esta pergunta? As respostas existentes serão mantidas, mas a pergunta não será mais visível.';
+$string['moveup'] = 'Mover para cima';
+$string['movedown'] = 'Mover para baixo';
+// ...
+
+// Relatórios e Exportação
+$string['reports'] = 'Relatórios';
+$string['noresponses'] = 'Ainda não há respostas.';
+$string['submitted'] = 'Data de Submissão';
+$string['downloadreport'] = 'Descarregar Relatório';
+$string['exportcsv'] = 'Exportar para CSV';
+
